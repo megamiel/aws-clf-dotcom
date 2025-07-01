7e25bbbc1c5555a132d717a6c927f658af8dfbc3
megamiel <hrydan4779@gmail.com>
2025-07-01 10:37:51 +0900

    test

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/aws-clf-dotcom/sw.js', { scope: '/aws-clf-dotcom/' })})}
